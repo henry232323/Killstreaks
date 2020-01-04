5a0a8116212accd021ae3015ed77e83c30e78eba
henry232323 <henry232323@users.noreply.github.com>
2020-01-04 02:34:44 -0700

    A working version!

--- src/main/java/henry232323/killstreaks/Killstreaks.java
+++ src/main/java/henry232323/killstreaks/Killstreaks.java
@@ -1,7 +1,10 @@
 package henry232323.killstreaks;
 
 import com.destroystokyo.paper.Title;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -32,8 +35,8 @@ public final class Killstreaks extends JavaPlugin implements Listener {
         config = getConfig();
 
         List<String> worldnames = config.getStringList("worlds");
-        if (worlds == null) {
-            worldnames = new ArrayList<String>();
+        if (worldnames.size() == 0) {
+            worldnames = new ArrayList<>();
             config.options().copyDefaults(true);
         }
 
@@ -52,6 +55,7 @@ public final class Killstreaks extends JavaPlugin implements Listener {
         Player killer = event.getEntity().getKiller();
         if (killer != null) {
             World eworld = event.getEntity().getWorld();
+            Player killed = event.getEntity();
 
             if (worlds.contains(eworld)) {
                 if (!streaks.containsKey(killer))
@@ -63,11 +67,11 @@ public final class Killstreaks extends JavaPlugin implements Listener {
                 if (titleMessages.size() > 0) {
                     Title t;
                     if (titleMessages.size() == 1) {
-                        t = new Title(String.format(titleMessages.get(0), killer.getDisplayName()));
+                        t = new Title(String.format(titleMessages.get(0), killer.getDisplayName(), killed.getDisplayName()));
                     } else {
                         t = new Title(
-                                String.format(titleMessages.get(0), killer.getDisplayName()),
-                                String.format(titleMessages.get(1), killer.getDisplayName()));
+                                String.format(titleMessages.get(0), killer.getDisplayName(), killed.getDisplayName()),
+                                String.format(titleMessages.get(1), killer.getDisplayName(), killed.getDisplayName()));
                     }
                     for (Player mplayer : eworld.getPlayers()) {
                         mplayer.sendTitle(t);
@@ -76,7 +80,7 @@ public final class Killstreaks extends JavaPlugin implements Listener {
 
                 String chatFmt = getConfig().getString("chat_messages." + killCount);
                 if (chatFmt != null) {
-                    String chatMessage = String.format(chatFmt, killer.getDisplayName());
+                    String chatMessage = String.format(chatFmt, killer.getDisplayName(), killed.getDisplayName());
                     for (Player mplayer : eworld.getPlayers()) {
                         mplayer.sendMessage(chatMessage);
                     }
@@ -85,10 +89,55 @@ public final class Killstreaks extends JavaPlugin implements Listener {
                 List<String> commands = getConfig().getStringList("commands." + killCount);
                 if (commands.size() != 0) {
                     for (String command : commands) {
-                        getServer().dispatchCommand(getServer().getConsoleSender(), command);
+                        getServer().dispatchCommand(getServer().getConsoleSender(), String.format(command, killer.getDisplayName(), killed.getDisplayName()));
                     }
                 }
             }
+        }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        try {
+            if (command.getName().equalsIgnoreCase("killstreaks")) {
+                if (args.length == 0) {
+                    String version = getDescription().getVersion();
+                    sender.sendMessage(ChatColor.GOLD + "Killstreaks Version " + version);
+                    return true;
+                } else if (args.length != 1) {
+                    return false;
+                }
+                if (args[0].equalsIgnoreCase("reload")) {
+                    File dfolder = getDataFolder();
+                    if (!dfolder.exists()) {
+                        dfolder.mkdir();
+                    }
+
+                    config = getConfig();
+
+                    List<String> worldnames = config.getStringList("worlds");
+                    if (worlds == null) {
+                        worldnames = new ArrayList<String>();
+                        config.options().copyDefaults(true);
+                    }
+
+                    worlds = new ArrayList<>();
+                    for (String w : worldnames) {
+                        World cworld = getServer().getWorld(w);
+                        if (cworld != null)
+                            worlds.add(cworld);
+                        else
+                            getLogger().warning("Cannot enable killstreaks in world " + w + ", because it does not exist!");
+                    }
+                    sender.sendMessage(ChatColor.GOLD + "Killstreaks reloaded");
+                    return true;
+                }
+            }
+
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
     }
 }
